58050d140f009605dc28e4a77dbd8f8d4fa06bcb
sanjay <sanjaysharma28111997@gmail.com>
2023-08-14 09:21:51 +0530

    newchanges

--- src/index.js
+++ src/index.js
@@ -20,4 +20,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-ReactDOM.render(<h1>Hello world!</h1>,document.getElementById('root'));
+ReactDOM.render(<h1>Hello world!</h1>,
+  document.getElementById('root'));
